8a24e3d336630cb15087ece8dec59871b759afad
Devin Stanley <DEVIN.STANLEYYY@GMAIL.COM>
2025-08-04 18:12:38 -0700

    Add social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './Navbar.css'
 
 const NavBar = () => {
     return (
         <nav className='navbar'>
-            <Link className='home-icon' to="/">Home</Link>
-            <Link className='nav-link' to="/projects">Projects</Link>
-            <Link className='nav-link' to="/about">About</Link>
+            <div className='nav-links'>
+                <NavLink className='nav-link' to="/">Home</NavLink>
+                <NavLink className='nav-link' to="/projects">Projects</NavLink>
+                <NavLink className='nav-link' to="/about">About</NavLink>
+            </div>
+            <div className="social-icons">
+                <a href="https://github.com/devinstanley" target="_blank" rel="noopener noreferrer" aria-label='GitHub'>
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/devin-stanley1/" target="_blank" rel="noopener noreferrer" aria-label='LinkedIn'>
+                    <FaLinkedin />
+                </a>
+            </div>
         </nav>
     );
 };
